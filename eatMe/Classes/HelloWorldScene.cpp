ec1de8d47710300d6e23814031ee2e93d63a0cb6
spuch <spuch@qq.com>
2014-11-22 23:23:32 +0800

    add .gitignore

--- eatMe/Classes/HelloWorldScene.cpp
+++ eatMe/Classes/HelloWorldScene.cpp
@@ -66,7 +66,7 @@ bool HelloWorld::init()
     this->addChild(label, 1);
 
     // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+    auto sprite = Sprite::create("hungryfish9.png");
 
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
